c954b4d4ae271237b5e798d14c2c4eb9734abc75
PhilippMeissner <pm@zeit.io>
2014-07-03 21:12:20 +0200

    released version 1.1 (check changelog)

--- Source.cpp
+++ Source.cpp
@@ -4,25 +4,28 @@ using namespace std;
 
 
 int main () {
-	HWND hwnd = FindWindow( NULL, "Diablo III");
+	HWND hwnd = FindWindow("D3 Main Window Class", "Diablo III");
+	bool bWindowVisibility = IsWindowVisible(hwnd);
 
-	cout << "\t\t\tDiablo 3 - Hider by Tracky\n\n\n";
+	cout << "\t\t\tDiablo 3 - Hider by v.1.1 Tracky\n\n\n";
 	cout << "Numpad 8 -> Hide Diablo 3\n";
 	cout << "Numpad 9 -> Show Diablo 3\n";
 	cout << "Numpad 0 -> Close program\n";
 
 	while (hwnd) {
 
-		if (GetAsyncKeyState(VK_NUMPAD8) & 0x8000) {
+		if (bWindowVisibility && (GetAsyncKeyState(VK_NUMPAD8) & 0x8000)) {
 			ShowWindow(hwnd, SW_HIDE);
 			cout << "WindowState = Hidden\n";
-			Sleep(500);
+			bWindowVisibility = FALSE;
+			Sleep(100);
 		}
 
-		if (GetAsyncKeyState(VK_NUMPAD9) & 0x8000) {
+		if (!bWindowVisibility && (GetAsyncKeyState(VK_NUMPAD9) & 0x8000)) {
 			ShowWindow(hwnd, SW_SHOW);
 			cout << "WindowState = Visible\n";
-			Sleep(500);
+			bWindowVisibility = TRUE;
+			Sleep(100);
 		}
 
 		if (GetAsyncKeyState(VK_NUMPAD0) & 0x8000) {
